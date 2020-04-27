68e8cb84bb129943c487b377f1c9b2b0834a4217
Kathleen McKiernan <851@holbertonschool.com>
2020-04-27 21:22:19 +0000

    add 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if cycle exists
+ * @list: singly linked list being checked
+ * Return: 0 on success
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *check;
+
+	if (list && list->next)
+		check = list->next;
+	else
+		return (0);
+
+	while (list && check)
+	{
+		if (list == check)
+			return (1);
+
+		if (check->next)
+			check = check->next;
+		else
+			return (0);
+
+		if (check->next)
+			check = check->next;
+		else
+			return (0);
+	}
+	return (0);
+}
