80cd770c94363ee821bc9efdca19164090e1b2c9
Kathleen McKiernan <851@holbertonschool.com>
2020-05-01 03:44:52 +0000

    edit for project requirements

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         elif ops is "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         else:
-            print("{}".format("Unknown operator. Available operators: +, -, * and /"))
+            print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
