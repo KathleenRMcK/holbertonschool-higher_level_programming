7b666e8342ccdc925005d18a77cc8a7d5b7e0c6d
Kathleen McKiernan <851@holbertonschool.com>
2020-05-18 23:50:08 +0000

    add 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+
+    put = 0
+
+    for help in range(x):
+        try:
+            print(my_list[help], end="")
+            put += 1
+        except:
+            None
+    print()
+    return put
