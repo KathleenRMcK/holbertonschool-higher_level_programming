4b1b74074399863377e8d9ea18522c3ece90f0cf
Kathleen McKiernan <851@holbertonschool.com>
2020-05-18 23:58:41 +0000

    add 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+
+    put = 0
+
+    for help in range(x):
+        try:
+            print("{:d}".format(my_list[help]), end="")
+            put += 1
+        except TypeError:
+            None
+        except ValueError:
+            None
+    print()
+    return put
