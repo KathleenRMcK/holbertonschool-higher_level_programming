bc36d191612fd80ea268905c114e7d724a2ad7d4
Kathleen McKiernan <851@holbertonschool.com>
2020-05-19 00:17:17 +0000

    add 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        num_res = a / b
+    except:
+        num_res = None
+    finally:
+        print("Inside result: {}".format(num_res))
+    return num_res
