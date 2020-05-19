b42a8722e7df839eb9d485b5aaf9a955961fba4b
Kathleen McKiernan <851@holbertonschool.com>
2020-05-19 00:17:32 +0000

    add 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+
+    listy = []
+
+    for help in range(list_length):
+        try:
+            holder = my_list_1[help] / my_list_2[help]
+
+        except TypeError:
+            print("wrong type")
+            holder = 0
+
+        except ZeroDivisionError:
+            print("division by 0")
+            holder = 0
+
+        except IndexError:
+            print("out of range")
+            holder = 0
+
+        finally:
+            listy.append(holder)
+    return listy
