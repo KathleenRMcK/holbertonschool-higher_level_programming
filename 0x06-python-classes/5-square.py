c17a4dea680a33b8122626af0677ae856dc86547
Kathleen McKiernan <851@holbertonschool.com>
2020-05-31 23:51:10 +0000

    update comments

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"""
+Define class Square
+"""
+
+
+class Square:
+    """is square"""
+
+    def __init__(self, size=0):
+        """inits square"""
+        self.__size = size
+
+    def area(self):
+        """area of square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """prints it all"""
+        if self.__size == 0:
+            print("")
+        else:
+            for help in range(self.__size):
+                print("#" * self.__size)
+
+    @property
+    def size(self):
+        """size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """get size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
