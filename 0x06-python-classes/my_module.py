d074e18796c6fb6d0e9bc3fc6a701d882e232239
KathleenRMcK <50117763+KathleenRMcK@users.noreply.github.com>
2020-05-31 18:01:53 -0400

    Delete my_module.py

--- 0x06-python-classes/my_module.py
+++
@@ -1,8 +0,0 @@
-"""Module for Square"""
-
-
-"""My Class"""
-class MyClass:
-    def __init__(self, my_square=0, my_function=0):
-        self.my_square = Square()
-        self.my_function = my_function()
