7b478ea01ff6829bf3c51cad6c7bfb5a6785694e
Kathleen McKiernan <851@holbertonschool.com>
2020-05-31 23:43:08 +0000

    update comments 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+Define class Square
+"""
+
+
 class Square:
-    __size = 0
+    """is a square"""
 
     def __init__(self, size=0):
+        """inits square"""
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
