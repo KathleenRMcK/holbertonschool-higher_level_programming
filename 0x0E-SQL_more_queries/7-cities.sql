c15652e4817eb2aaccce9883c96b8fa47bc02fb9
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-07-14 17:20:07 -0400

    add 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,1 +1,7 @@
--- Creates the database and table on your server with specifications
+-- Creates database and table on server with specifications
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(256) NOT NULL, state_id INT NOT NULL,
+FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
