8bc659b67cc34f1e1831eac3741f2d1de979f23d
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-07-14 16:52:22 -0400

    rework table call

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Creates database and table on server with specifications
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE DATABASE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS states (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
