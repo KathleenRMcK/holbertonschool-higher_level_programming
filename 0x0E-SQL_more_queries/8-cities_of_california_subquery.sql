a30fe1a1ee65a13674e3db62734de138e88ebfbe
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-07-14 17:45:30 -0400

    remove unneccessary char

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Lists all the cities that can be found in the database
-SELECT id, name FROM cities WHERE state_id == (
+SELECT id, name FROM cities WHERE state_id = (
 SELECT id FROM states WHERE name = "California")
 ORDER BY cities.id;
