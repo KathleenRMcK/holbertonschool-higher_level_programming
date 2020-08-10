1f8981f87813b99854797532867c620a55755a45
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-08-09 22:38:59 -0400

    sync for checker

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
                               user=sys.argv[1],
                               passwd=sys.argv[2],
                               db=sys.argv[3])
-    cur_help = db.cursor()
+    cur_help = db_help.cursor()
     cur_help.execute("SELECT * FROM states ORDER BY id asc")
     states = cur_help.fetchall()
     for navigate in states:
