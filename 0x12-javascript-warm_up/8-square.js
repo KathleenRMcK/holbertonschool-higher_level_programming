6d1ab6038da73e1a7d05f3cd0f140839090557a4
Kathleen McKiernan <mckiernan.katie@gmail.com>
2020-09-15 23:08:43 -0400

    sync for checks

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 if (isNaN(parseInt(process.argv[2]))) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
   const total = parseInt(process.argv[2])
   for (let helper = 0; helper < total; helper++) {
-    console.log('X'.repeat(total))
+    console.log('X'.repeat(total));
   }
 }
