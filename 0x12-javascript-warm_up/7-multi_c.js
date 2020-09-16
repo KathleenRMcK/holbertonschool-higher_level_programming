b91c6c381533c7adeb77104ed5ec16cfbdb3bcd5
Kathleen McKiernan <mckiernan.katie@gmail.com>
2020-09-15 23:04:45 -0400

    sync for checks

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
   const total = parseInt(process.argv[2]);
-  for (let helper = 0; holder < total; holder++) {
+  for (let holder = 0; holder < total; holder++) {
     console.log('C is fun');
   }
 }
