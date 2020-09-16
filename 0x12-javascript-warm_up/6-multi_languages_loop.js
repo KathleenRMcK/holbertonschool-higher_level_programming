70594d80db06c24501b4912d2b1933fe7f8fd52c
Kathleen McKiernan <mckiernan.katie@gmail.com>
2020-09-15 23:00:57 -0400

    sync for checks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let holder = 0; holder < array.length; holder += 1) {
-  console.log(holder);
+  console.log(array[holder]);
 }
